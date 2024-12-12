85e592342bf193a49d8870477bf95ddacd4c37c2
Johan Larsson <johan.larsson@ub.gu.se>
2024-12-12 13:57:01 +0100

    temp landing

--- app.vue
+++ app.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="wrapper">
+    <div class="container">
+      <h1>Hello Dalsland</h1>
+      <p><small>Coming soonish....</small></p>
+    </div>
   </div>
 </template>
+
+<style>
+.wrapper {
+  margin: 0;
+  display: flex;
+  height: 100vh;
+  align-items: center;
+}
+.container {
+  padding: 20px;
+  width: 85vw;
+  margin: 0 auto;
+  overflow: auto;
+  background-color: #002147; /* oxford blue */
+  color: whitesmoke;
+}
+
+h1 {
+  font-size: 3em;
+}
+</style>
